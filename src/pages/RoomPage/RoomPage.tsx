1079516568d97e95a0fc9f73ce259333756ede79
Anurag <anuragmarketing101@gmail.com>
2023-03-29 20:00:55 +0530

    Add: deleteMessage func

--- src/pages/RoomPage/RoomPage.tsx
+++ src/pages/RoomPage/RoomPage.tsx
@@ -8,7 +8,7 @@ const RoomPage = () => {
   const { roomID } = useParams();
   const [roomDetails, setRoomDetails] = useState<IRoom | null>(null);
   const [messages, setMessages] = useState<any>([]);
-  const [messageContent, setMessageContent] = useState <string> ("")
+  const [messageContent, setMessageContent] = useState<string>("");
   const { userDetails } = useContext(AppContext);
 
   const fetchRoom = async () => {
@@ -31,13 +31,19 @@ const RoomPage = () => {
 
     console.log(res);
 
-    setMessageContent("")
+    setMessageContent("");
+  };
+
+  const deleteMessage = async (id: number) => {
+    const data = await supabase.from("messages").delete().eq("id", id);
+
+    console.log(data);
   };
 
   const fetchRoomMessages = async () => {
     const res = await supabase.from("messages").select().eq("roomID", roomID);
     console.log(`fetchRoomMessages`);
-    console.log(res)
+    console.log(res);
     if (res.error === null) setMessages(res?.data);
   };
 
@@ -47,20 +53,37 @@ const RoomPage = () => {
       .channel("any")
       .on(
         "postgres_changes",
-        { event: "INSERT", schema: "public", table: "messages",},
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "messages",
+          filter: `roomID=eq.${roomID}`,
+        },
         (payload) => {
-          console.log("Change received!", payload);
-          if(payload.errors === null) {
-            setMessages((prevMessages: any) => [...prevMessages, payload?.new])
-            console.log(messages)
+          console.log("INSERT Change received!", payload);
+          if (payload.errors === null) {
+            setMessages((prevMessages: any) => [...prevMessages, payload?.new]);
+            console.log(messages);
           }
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "DELETE",
+          schema: "public",
+          table: "messages",
+          filter: `roomID=eq.${roomID}`,
+        },
+        (payload) => {
+          console.log("DELETE Change received!", payload);
+          if(payload.errors === null) setMessages((prevMessages: any) => prevMessages.filter((message: any) => message.id !== payload.old.id) );
         }
       )
       .subscribe();
   }, []);
 
-//  filter: `roomID=eq.${roomID}`
-
+  //  filter: `roomID=eq.${roomID}`
 
   useEffect(() => {
     fetchRoom();
@@ -70,17 +93,33 @@ const RoomPage = () => {
   return (
     <div className="w-full h-full  flex flex-col items-center justify-between">
       {/* <h1 className="text-4xl"> {roomDetails?.name} </h1> */}
-      <button onClick={() => console.log(messages)} > LOG MESSAGES </button>
+      <button onClick={() => console.log(messages)}> LOG MESSAGES </button>
 
       <div className="w-full h-full overflow-x-hidden overflow-y-auto flex flex-col items-center justify-start bg-green-600 py-10">
         {messages &&
           messages?.map((message: any) => {
-            return <p key={message.id} className="p-2 m-2 text-2xl font-medium border-2 border-black"> {message.content} </p>;
+            return (
+              <div key={message.id} className="p-2 m-2 space-x-3">
+                <p className="text-2xl font-medium border-2 border-black">
+                  {" "}
+                  {message.content}{" "}
+                </p>
+                <button onClick={() => deleteMessage(message?.id)}>
+                  {" "}
+                  Delete{" "}
+                </button>
+              </div>
+            );
           })}
       </div>
 
       <div className="w-full flex justify-between items-center p-5 bg-green-200">
-        <input type="text" placeholder="Type your message here...." value={messageContent} onChange={(e: any) => setMessageContent(e.target.value)} />
+        <input
+          type="text"
+          placeholder="Type your message here...."
+          value={messageContent}
+          onChange={(e: any) => setMessageContent(e.target.value)}
+        />
         <button onClick={sendMessage}>Send</button>
 
         {/* <button onClick={fetchRoomMessages}>fetchRoomMessages</button> */}
