f3858edfb21ee8f543c9f72d54a151c3fac9b70f
Anurag <anuragmarketing101@gmail.com>
2023-03-30 04:22:28 +0530

    Add: vercel analytics :)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./App.css";
 import Layout from "./Layouts/Layout";
 import AppRoutes from "./setup/routes-manager/index";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from '@vercel/analytics';
 
 function App() {
+  inject()
   return (
     <BrowserRouter>
       <Layout>
